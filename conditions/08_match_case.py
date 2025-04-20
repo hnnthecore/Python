fb8152976ca33c414fb5529aa5ab5d7a99a58fa8
unknown <Hanny_it_01@WIN-IT-01.hanny_it.local>
2025-04-20 22:33:07 +0200

    finisch conditions

---
+++ conditions/08_match_case.py
@@ -0,0 +1,9 @@
+# Exercise 08: Number Range Check
+
+number = int(input("Enter a number: "))
+
+# Check if the number is in the range 1 to 100 (inclusive)
+if 1 <= number <= 100:
+    print("The number is within the range 1 to 100.")
+else:
+    print("The number is outside the range.")
